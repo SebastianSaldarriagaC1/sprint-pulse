5830c63a0e19ac0ba24bf2b2fd5b3ed29e94214b
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-06-26 00:47:17 -0500

    Add user entity

--- sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/SprintPulseApiApplication.java
+++ sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/SprintPulseApiApplication.java
@@ -2,8 +2,10 @@ package com.ssaldarriaga.sprintpulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SprintPulseApiApplication {
 
 	public static void main(String[] args) {
