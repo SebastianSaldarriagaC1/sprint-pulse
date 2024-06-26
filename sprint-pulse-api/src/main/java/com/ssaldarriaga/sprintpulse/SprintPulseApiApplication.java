d7f8366c5ab805f287dd8b8c35c747de2a907d71
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-06-25 22:53:44 -0500

    first commit

---
+++ sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/SprintPulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssaldarriaga.sprintpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintPulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintPulseApiApplication.class, args);
+	}
+
+}
---
+++ sprint-pulse-api/src/test/java/com/ssaldarriaga/sprintpulse/SprintPulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssaldarriaga.sprintpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintPulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
