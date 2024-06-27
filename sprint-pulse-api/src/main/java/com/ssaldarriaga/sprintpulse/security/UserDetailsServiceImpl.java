1956948f8a5e110196c6181620cf9975d34627fc
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-06-27 10:45:59 -0500

    Add JWT Authentiacion configuration

---
+++ sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ssaldarriaga.sprintpulse.security;
+
+import com.ssaldarriaga.sprintpulse.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+    }
+}
