8fc5590b8f43b6169ebf961247e63da498261fab
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-06-27 09:24:55 -0500

    Add roles entity and relationship

--- sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/user/User.java
+++ sprint-pulse-api/src/main/java/com/ssaldarriaga/sprintpulse/user/User.java
@@ -1,17 +1,20 @@
 package com.ssaldarriaga.sprintpulse.user;
 
+import com.ssaldarriaga.sprintpulse.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -51,6 +54,12 @@ public class User implements UserDetails, Principal {
     @Column(insertable = false)
     private LocalDateTime lastModifiedDate;
 
+    // Relationships
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
+
+
+    // UserDetails methods
     @Override
     public String getName() {
         return email;
@@ -58,7 +67,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream()
+                .map(r -> new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
