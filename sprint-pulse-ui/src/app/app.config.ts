f0ad0df5455e8128da4f3fd152be5ae7b1d090b6
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-08-13 18:33:40 -0500

    feat: Add basic reposnsity to registration page

--- sprint-pulse-ui/src/app/app.config.ts
+++ sprint-pulse-ui/src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// TODO: Import HttpClientModule (Research how to do this for Angular 18)
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
